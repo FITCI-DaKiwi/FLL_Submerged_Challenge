b4607316a7e0a07e79b75647ca6750cc7449abed
Alexloveslego <bobolovesalex@gmail.com>
2024-11-21 10:45:59 -0500

    Add files via upload

---
+++ New_11212024_menu.py
@@ -0,0 +1,67 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+
+# Let's offer these menu options. You can add as many as you like.
+menu_options = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+# Normally, the center button stops the program. But we want to use the
+# center button for our menu. So we can disable the stop button.
+
+pressed = []
+hub.system.set_stop_button(None)
+menu_index = 0
+
+while True:
+
+    hub.display.char(menu_options[menu_index])
+
+    # Wait for any button.
+    pressed = ()
+    while not pressed:
+        pressed = hub.buttons.pressed()
+        wait(10)
+    
+    # Wait for the button to be released.
+    while hub.buttons.pressed():
+        wait(10)
+
+    # Now check which button was pressed.
+    if Button.CENTER in pressed:
+        # Center button, so the menu is done!
+        break
+    elif Button.LEFT in pressed:
+        # Left button, so decrement menu menu_index.
+        menu_index = (menu_index + 1) % len(menu_options)
+    elif Button.RIGHT in pressed:
+        # Right button, so increment menu menu_index.
+        menu_index = (menu_index - 1) % len(menu_options)
+
+# Now we want to use the Center button as the stop button again.
+hub.system.set_stop_button(Button.CENTER)
+
+# Based on the selection, choose a program.
+selected = menu_options[menu_index]
+if selected == "1":
+    import Run_1_collection
+elif selected == "2":
+    import Run_2_Second_run_Sonar_green_circle
+elif selected == "3":
+    import mission1_Taotao
+elif selected == "4":
+    import Run_4_kraken
+elif selected == "5":
+    import Run_5_Reef_Segments
+elif selected == "6":
+    import Run_6_11192024_Scuba_Diver
+elif selected == "7":
+    import Run_7_Sending_the_Samples
+elif selected == "8":
+    import Run_8_Mission_15_and_Delivering_the_Shark_6
+elif selected == "9":
+    import Run_9_whale_dot_py_taste_good
